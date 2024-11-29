dbb54d15f3a40c5ff9e6f8fa4d79c633fa1f2e1e
Aaron23009 <147981742+Aaron23009@users.noreply.github.com>
2024-11-29 21:29:40 +0530

    Egg

---
+++ core/src/main/java/io/github/approject/ap_project/Birds/Egg.java
@@ -0,0 +1,12 @@
+package io.github.approject.ap_project.Birds;
+
+import com.badlogic.gdx.physics.box2d.*;
+
+public class Egg extends Bird {
+
+    public Egg(){
+        super("egg.png");
+
+    }
+}
+
---
+++ core/src/main/java/io/github/approject/ap_project/Birds/Yellow.java
@@ -0,0 +1,8 @@
+package io.github.approject.ap_project.Birds;
+
+public class Yellow extends Bird {
+    float PPM = 100f;
+    public Yellow() {
+        super("yellow.png");
+    }
+}
