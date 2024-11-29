f493efc35e113603248ec5f27c9f1551db1fd190
Aaron23009 <147981742+Aaron23009@users.noreply.github.com>
2024-11-29 21:30:47 +0530

    Updated Birds

--- core/src/main/java/io/github/approject/ap_project/Birds/White.java
+++ core/src/main/java/io/github/approject/ap_project/Birds/White.java
@@ -1,7 +1,15 @@
 package io.github.approject.ap_project.Birds;
 
+import com.badlogic.gdx.physics.box2d.*;
+
 public class White extends Bird {
+    float PPM = 100f;
+
     public White() {
         super("white.png");
+        this.canlayegg = true;
+    }
+    public Body layEgg(World world, float x, float y){
+        return getEggBody(world, x, y);
     }
 }
