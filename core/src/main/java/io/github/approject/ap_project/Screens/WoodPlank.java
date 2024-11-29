52b263018d9a1754800131c3974f3ba35fbe076b
Aaron23009 <147981742+Aaron23009@users.noreply.github.com>
2024-11-29 21:46:30 +0530

    Add files via upload

---
+++ core/src/main/java/io/github/approject/ap_project/Screens/WoodPlank.java
@@ -0,0 +1,9 @@
+package io.github.approject.ap_project.Screens;
+
+import io.github.approject.ap_project.Materials.Wood;
+
+public class WoodPlank extends Wood {
+    public WoodPlank() {
+        super("woodplank.png");
+    }
+}
